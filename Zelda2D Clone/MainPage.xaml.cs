30f53dccab1591608128b50d0d5d53e405888819
CrispyBoardgames <44710114+CrispyBoardgames@users.noreply.github.com>
2018-11-20 08:44:05 -0800

    Update MainPage.xaml.cs

--- Zelda2D Clone/MainPage.xaml.cs
+++ Zelda2D Clone/MainPage.xaml.cs
@@ -28,6 +28,20 @@ namespace Zelda2D_Clone
             this.InitializeComponent();
             
         }
+
+        /* The following "Clicked_YY" methods are used for moving the player. 
+         * There are four buttons, one for each method in the MainPage.xaml
+         * The current iteration for moving the player changes the margins of an image each time a "Clicked_YY" method is clicked. 
+         * Margin is of type Thickness so we call a new Thickness constructor. The margin is constructed from a "Top," a "Bottom," a "Left," and a "Right."
+         * We change the value of these to change where an image is located.
+         * 
+         * Bugs:
+         *      11/20/2018
+         *      - The image disappears when moving "out of range." Unsure what the range is. 
+         *          Doesn't seem to be the window area as it disappears before reaching the end of the screen.
+         *      - At some point, when positioned at an arbritrary point on the grid, the "Up" and "Down" buttons stoppped working.
+         *          Have not gotten this bug to occur after repositioning the image.
+         */
         private void Clicked_Up(object sender, RoutedEventArgs e)
         {
             Player.Margin = new Thickness(Player.Margin.Left, Player.Margin.Top - 20.0, Player.Margin.Right, Player.Margin.Bottom - 20.0);
