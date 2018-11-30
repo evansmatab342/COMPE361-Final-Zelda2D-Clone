b0d67af5b99c06644ea037bba357520c193cdf1e
evansmatab342 <44710418+evansmatab342@users.noreply.github.com>
2018-11-30 13:49:42 -0800

    Stats V0.2
    Added more to basic structure

--- Zelda2D Clone/Stats/Statistics.cs
+++ Zelda2D Clone/Stats/Statistics.cs
@@ -8,6 +8,20 @@ namespace Zelda2D_Clone.Stats
 {
     class Being
     {
+        int attackSpeed; //To decide order of attack upon intial encounter
+        int attackPower; //Will decide amount of damage being done per attack
+        int defense; //Determines effectiveness of enemy attacks
+        int beingHealth; //Stores information on 
 
+    }
+
+    public class Player : Being
+    {
+        //TODO 
+    }
+
+    public class NPC : Being
+    {
+        //TODO 
     }
 }
