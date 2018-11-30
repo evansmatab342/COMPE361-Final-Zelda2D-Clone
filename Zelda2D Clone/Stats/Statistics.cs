f50b1d235cbcfd2665a0e79c97165c540723cedf
evansmatab342 <44710418+evansmatab342@users.noreply.github.com>
2018-11-30 14:18:29 -0800

    Stats V0.3
    Changed accessibility

--- Zelda2D Clone/Stats/Statistics.cs
+++ Zelda2D Clone/Stats/Statistics.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace Zelda2D_Clone.Stats
 {
-    class Being
+    public class Being
     {
-        int attackSpeed; //To decide order of attack upon intial encounter
-        int attackPower; //Will decide amount of damage being done per attack
-        int defense; //Determines effectiveness of enemy attacks
-        int beingHealth; //Stores information on 
+        float attackSpeed; //To decide order of attack upon intial encounter
+        float attackPower; //Will decide amount of damage being done per attack
+        float defense; //Determines effectiveness of enemy attacks
+        float currentHealth; //Stores information on 
+        float maxHealth;
 
     }
 
